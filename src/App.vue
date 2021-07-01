af2c9a1cd199edcd71b0998f6f8a72e7ba01bfee
tari404 <2680383605@qq.com>
2021-07-01 18:55:09 +0800

    ⚙️ init homepage

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Nav />
     <router-view />
   </div>
 </template>
 
+<script>
+import Nav from '@/components/Nav'
+
+export default {
+  name: 'App',
+  components: {
+    Nav,
+  },
+}
+</script>
+
 <style lang="stylus">
 #app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+  position relative
+  min-width 1440px
 </style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav id="astro-nav">
+    <div class="main-title _fm">Astropia</div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'AstropiaNav',
+}
+</script>
+
+<style lang="stylus">
+#astro-nav
+  position absolute
+  top 0
+  left 0
+  height 150px
+  width 100%
+
+  .main-title
+    position absolute
+    top 16px
+    left 90px
+    font-size 100px
+</style>
