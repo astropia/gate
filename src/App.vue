d6dabbbcacfe77ae4c721a5dac5e0caef45922d7
tari404 <2680383605@qq.com>
2021-08-10 15:48:24 +0800

    ⚙️ update homepage

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,22 @@
 <template>
   <div id="app">
-    <Nav />
+    <!-- <Nav /> -->
     <router-view />
   </div>
 </template>
 
 <script>
-import Nav from '@/components/Nav'
+// import Nav from '@/components/Nav'
 
 export default {
   name: 'App',
   components: {
-    Nav,
+    // Nav,
   },
 }
 </script>
 
 <style lang="stylus">
 #app
-  padding-top 150px
-  min-width 1440px
   position relative
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,64 +1,136 @@
 <template>
-  <div class="home">
-    <div class="home-card">
-      <router-link to="/heros">
-        <div class="card">
-          <span class="_fm">Heros</span>
-        </div>
-      </router-link>
-      <p>
-        这里是你召唤英雄的地方，你将随机抽取到不同种族，不同职业，不同能力值，不同稀有度的英雄NFT，这些英雄NFT将是未来你进入各种不同NFT游戏的钥匙。现在英雄池试验性的免费开放，你只需要支付很少的gas费就可以获得自己的英雄。
-      </p>
-      <div class="button">关于英雄稀有度</div>
+  <div id="homepage">
+    <div class="home-banner">
+      <div class="title">
+        <img src="@/assets/logo.png" width="330" alt="logo" />
+        <div class="_fm">Creator Metaverse</div>
+        <p>with NFT Games</p>
+      </div>
+      <div class="bg"></div>
     </div>
-    <div class="home-card">
-      <router-link to="/explore">
-        <div class="card">
-          <span class="_fm">Explore</span>
-        </div>
-      </router-link>
+
+    <div class="home-story" :style="{ transform: `translateY(${-scrollY * 0.09}px)` }">
       <p>
-        持有英雄NFT才可以进入探索世界，这里有无尽的宝藏：装备、特定英雄、剧情卡牌等等。不要太过惊讶，该区域同样也是免费开放的，欢迎来到Astropia宇宙。
+        Dungeons & Dragons (commonly abbreviated as D&D or DnD) is a fantasy tabletop role-playing game (RPG) originally
+        designed by Gary Gygax and Dave Arneson.It was first published in 1974 by Tactical Studies Rules, Inc. (TSR).It
+        has been published by Wizards of the Coast (now a subsidiary of Hasbro) since 1997. The game was derived from
+        miniature wargames, with a variation of the 1971 game Chainmail serving as the initial rule system.D&D's
+        publication is commonly recognized as the beginning of modern role-playing games and the role-playing game
+        industry.
       </p>
+      <div class="bg _fm">The Story</div>
     </div>
+
+    <div class="home-mint"></div>
+
+    <div class="home-intro"></div>
+
+    <div class="home-roadmap"></div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Home',
+  data() {
+    return {
+      scrollY: window.scrollY,
+    }
+  },
+  created() {
+    window.addEventListener('scroll', this.onScroll)
+  },
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.onScroll)
+  },
+  methods: {
+    onScroll() {
+      this.scrollY = window.scrollY
+    },
+  },
 }
 </script>
 
-<style lang="stylus" scoped>
-.home
-  padding 140px 0
-  display flex
-  justify-content center
-.home-card
-  margin 0 26px
-  width 386px
-  .card
-    height 626px
-    background #282d30
-    border-radius 10px
-    border 1px solid rgba(#fff, 0.38)
-    position relative
-    cursor pointer
-  span
-    font-size 50px
+<style lang="stylus">
+#homepage
+  background-color #252131
+
+.home-banner
+  height 720px
+  overflow hidden
+  position relative
+  .title
     position absolute
-    left 22px
-    bottom 50px
+    top 200px
+    left 10%
+    display flex
+    flex-direction column
+    align-items center
+    >div
+      margin 18px 0 2px
+      font-size 18px
+      line-height 26px
+  .bg
+    width 100%
+    height 100%
+    background-image url('~@/assets/homepage/banner.jpg')
+    background-size cover
+    background-position center
+
+.home-story
+  padding-bottom 78px
+  background-color #252131
+  position relative
   p
-    margin-top 68px
-    padding 0 32px
-    font-size 16px
-    line-height 32px
-  .button
-    margin-top 32px
+    margin auto
+    max-width 800px
+    width 90%
+    color #fff
+    font-size 18px
+    line-height 22px
+    letter-spacing 0.1em
     text-align center
-    font-size 20px
-    line-height 32px
-    cursor pointer
+    position relative
+    top -10px
+    z-index 1
+  .bg
+    position absolute
+    top 0
+    left 0
+    width 100%
+    height 100%
+    background-color #252131
+    font-size 150px
+    color transparent
+    -webkit-text-stroke 1px rgba(#7851E7, .7)
+    letter-spacing 0.1em
+    display flex
+    justify-content center
+    align-items flex-end
+    &:before
+      content ''
+      border-left solid 100vw #252131
+      border-top solid 150px transparent
+      position absolute
+      right 0
+      top -150px
+
+.home-mint
+  height 700px
+  background-color #9DFBFF
+  position relative
+  &:before
+    content ''
+    border-left solid 100vw #9DFBFF
+    border-top solid 50px transparent
+    position absolute
+    right 0
+    top -50px
+
+.home-intro
+  height 460px
+
+.home-roadmap
+  height 660px
+  background-color #443868
 </style>
